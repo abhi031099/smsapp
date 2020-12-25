17612deb57e73bc7dc3134b5501ecd305e5ff5d4
abhi031099 <abhishek031099@gmail.com>
2020-12-25 22:36:11 +0530

    made a few logic changes in plot component

--- src/plots.js
+++ src/plots.js
@@ -39,7 +39,7 @@ class Plot extends Component{
     }
     render(){
         const { humidity } = this.state;
-        const humidata = humidity.field2;
+        const humidata = humidity.field3;
         let statHumi =" ";
        //for humidty  
        if(humidata>50){
@@ -49,7 +49,7 @@ class Plot extends Component{
        }
        //for temp
        const {temperature} = this.state
-       const tempdata = temperature.field3;
+       const tempdata = temperature.field2;
        let statTemp =" ";
        if(tempdata>60){
         statTemp = <p className="red-text center">DANGEROUS <i className="material-icons red-text">error</i></p>;
@@ -59,16 +59,16 @@ class Plot extends Component{
       // console.log("temp",tempdata)
        //for gas
        const {gaslevels} = this.state
-       const gasdata = gaslevels.field4;
+       const gasdata = gaslevels.field1;
        let statGas =" ";
-       if(gasdata<60){
+       if(gasdata>200){
         statGas = <p className="red-text center">DANGEROUS <i className="material-icons red-text">error</i></p>;
    } else{
     statGas = <p className="green-text center">SAFE <i className="material-icons green-text">done_outline</i></p>;
    }
    //for distance
    const {distance}= this.state
-   const distData = distance.field1;
+   const distData = distance.field4;
    let statDist = " ";
    if (distData<50){
     statDist = <p className="red-text center">Open<i className="material-icons red-text">error</i></p>;
